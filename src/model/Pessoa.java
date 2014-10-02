2d10d0426d40f8c076cf762103b1d12af78a680a
Matheus Costa de Castro <mthcastro@me.com>
2014-10-02 10:59:11 -0300

    Criação da Classe Pessoa no pacote Modelo

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,5 +1,47 @@
 package model;
 
-public class Pessoa {
+public abstract class Pessoa {
+	private int cpf;
+	private int nome;
+	private int email;
+	private int senha;
+	
+	public Pessoa() {
+		super();
+	}
 
+	public Pessoa(int cpf, int nome, int email, int senha) {
+		super();
+		this.setCpf(cpf);
+		this.setNome(nome);
+		this.setEmail(email);
+		this.setSenha(senha);
+	}
+	
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getNome() {
+		return nome;
+	}
+	public void setNome(int nome) {
+		this.nome = nome;
+	}
+	public int getEmail() {
+		return email;
+	}
+	public void setEmail(int email) {
+		this.email = email;
+	}
+	public int getSenha() {
+		return senha;
+	}
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	
 }
